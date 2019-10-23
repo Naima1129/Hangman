caf7bfe4c6cf9f71473b8147566d6be9ae548dd9
Naima1129 <56046989+Naima1129@users.noreply.github.com>
2019-10-23 13:23:49 -0700

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -6,7 +6,7 @@ print("Ok " + hi +", let's play hangman!")
 
 
 
-myWord = choice(["dogs"])
+myWord = choice(["dogs","fish","rocks"])
 myList = list(myWord)
 
 choice = input(" Type a word: ")
@@ -14,11 +14,13 @@ choice = input(" Type a word: ")
 if choice == myWord:
 	print("It's a match")
 	print (" You have obtained victory!")
+	
 else:
 	print("Sorry, that word isn't it ")
+
 guessedLetters =[]
 correctLetters= []
-word = []
+
 
 for a in myList:
 	correctLetters.append("_")
@@ -27,11 +29,11 @@ print(correctLetters)
 #how to check if a letter is in a word
 play = True
 
-if word == "dogs":
-	print("Congrats you won! ")
-	break
-while play:
 
+while play:
+	if correctLetters == myList:
+		print("Congratulations, you got the word! ")
+		break
 	letter = input(" Type a letter ")
 
 	if letter in myWord:
@@ -40,25 +42,40 @@ while play:
 		if myWord == "dogs":
 			if letter == "d":
 				correctLetters[0] = "d"
-				word.append("d")
-			
-
 			if letter == "o":
 				correctLetters[1] = "o"
-				word.append("o")
-
-
 			if letter == "g":
 				correctLetters[2] = "g"
-				word.append("g")
-
-
 			if letter == "s":
 				correctLetters[3] = "s"
-				word.append("s")
+		elif myWord == "fish":
+			if letter == "f":
+				correctLetters[0]="f"
+			if letter == "i":
+				correctLetters[1]="i"
+			if letter == "s":
+				correctLetters[2]="s"
+			if letter == "h":
+				correctLetters[3]="h"
+
+		else:
+			if letter == "r":
+				correctLetters[0]="r"
+			if letter == "o":
+				correctLetters[1]="o"
+			if letter == "c":
+				correctLetters[2]="c"
+			if letter == "k":
+				correctLetters[3]="k"
+			if letter == "s":
+				correctletters[4]="s"
+		
+
 
 			print(correctLetters)
+			print("Guessed letters: " +str(guessedLetters))
 
+			
 	
 
 	else:
